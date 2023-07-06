8e617dd57e313a6ab7fa1f423be6eba338e3f501
AntonBelonogov <belonog.2000@yandex.ru>
2023-07-07 03:35:19 +0700

    Import optimized

--- src/main/java/ru/isands/BackendTask/dto/FridgeDto.java
+++ src/main/java/ru/isands/BackendTask/dto/FridgeDto.java
@@ -7,10 +7,6 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Positive;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
--- src/main/java/ru/isands/BackendTask/dto/PhoneDto.java
+++ src/main/java/ru/isands/BackendTask/dto/PhoneDto.java
@@ -7,9 +7,6 @@ import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
